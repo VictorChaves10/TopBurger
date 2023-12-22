fec9063920cd2b1a76700571e31e5d4dfd2b1851
Victor Chaves <victor.chaves460@gmail.com>
2023-12-22 11:38:24 -0300

    Personalized the title of favorites and add new img

--- TopBurgers/TopBurgers/obj/Debug/net6.0/TopBurgers.AssemblyInfo.cs
+++ TopBurgers/TopBurgers/obj/Debug/net6.0/TopBurgers.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TopBurgers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c94ca7a819d94a83fab1eb88778735cec7fadbc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TopBurgers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TopBurgers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
