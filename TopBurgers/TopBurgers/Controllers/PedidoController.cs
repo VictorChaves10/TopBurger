77d378e729af5f03a4ec0423bbfca8f0998f103d
Victor Chaves <victor.chaves460@gmail.com>
2024-01-06 17:03:05 -0300

    feat: added the post method to the order

--- TopBurgers/TopBurgers/Controllers/PedidoController.cs
+++ TopBurgers/TopBurgers/Controllers/PedidoController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using TopBurgers.Models;
 using TopBurgers.Repositories.Interfaces;
 
@@ -23,7 +24,44 @@ namespace TopBurgers.Controllers
         [HttpPost]
         public IActionResult Checkout(Pedido pedido)
         {
+            int totalItensPedido = 0;
+            decimal precoTotalPedido = 0.0m;
+
+            List<CarrinhoCompraItem> items = _carrinhoCompra.GetCarrinhoCompraItems();
+            _carrinhoCompra.CarrinhoCompraItems = items;
+
+            if (_carrinhoCompra.CarrinhoCompraItems.Count > 0)
+            {
+
+                ModelState.AddModelError("", "Seu carrinho está vazio");
+
+            }
+
+            foreach (var item in items)
+            {
+                totalItensPedido += item.Quantidade;
+                precoTotalPedido += (item.Lanche.Preco * item.Quantidade);
+            }
+
+            pedido.TotalItensPedido = totalItensPedido;
+            pedido.PedidoTotal = precoTotalPedido;
+
+            if (ModelState.IsValid)
+            {
+                _pedidoRepository.CriarPedido(pedido);
+
+                ViewBag.CheckoutCompleto = "Obrigado pelo seu pedido";
+                ViewBag.TotalPedido = _carrinhoCompra.GetCarrinhoCompraTotal();
+
+                _carrinhoCompra.LimparCarrinho();
+
+                return View("~/Views/Pedido/CheckoutCompleto.cshtml", pedido);
+
+            }
+
             return View(pedido);
+
         }
+
     }
 }
