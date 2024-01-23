e03404ef17e4103096304a2ce2dc7a360d1da7e3
Victor Chaves <victor.chaves460@gmail.com>
2024-01-23 10:59:15 -0300

    feat: add login page

---
+++ TopBurgers/TopBurgers/Controllers/AccountController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TopBurgers.ViewModels;
+
+namespace TopBurgers.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new LoginViewModel()
+            {
+                ReturnUrl = returnUrl
+            });
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginVm)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(loginVm);
+            }
+
+            var user = await _userManager.FindByNameAsync(loginVm.UserName);
+
+            if (user == null)
+            {
+                var result = await _signInManager.PasswordSignInAsync(user, loginVm.Password, false, false);
+
+                if (result.Succeeded)
+                {
+                    if(string.IsNullOrEmpty(loginVm.ReturnUrl))
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    
+                    return Redirect(loginVm.ReturnUrl);
+                }
+            }
+
+            ModelState.AddModelError("", "Falha ao realizar o login!");
+
+            return View(loginVm);
+
+        }
+    }
+}
